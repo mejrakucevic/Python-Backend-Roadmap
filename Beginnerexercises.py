337276fa5c2b272f842073fb4a60250a5d4b6a68
mejrakucevic <mejrakucevic420@gmail.com>
2024-07-03 15:08:57 +0200

    .

--- Beginnerexercises.py
+++ Beginnerexercises.py
@@ -163,3 +163,49 @@ b = 150
 # print(s1.majorityElement(nums))
             
 # ----------------------------- 
+
+# 26. Remove Duplicates from Sorted Array
+
+# Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+# Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+# Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+# Return k.
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        # nums = [1,2,3,1,4,5]
+        
+        counter = {}
+        repeatedNum = None
+        
+        length = len(nums)
+        
+        
+        for i in list(nums):
+            if i in counter:
+                counter[i] += 1
+                nums.remove(i)
+            else: counter[i] = 0
+            
+        maxCount = 0
+        
+        for i, repeats in counter.items():
+            if repeats > maxCount:
+                maxCount == counter
+                repeatedNum = counter[i]
+                
+        print(nums)
+        
+            
+            
+s1 = Solution()
+
+s1.removeDuplicates([0,0,1,1,1,2,2,3,3,4])  
+
+# Example 1:
+# Input: nums = [1,1,2]
+# Output: 2, nums = [1,2,_]
+# Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
