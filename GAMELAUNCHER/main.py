b251af7e66a867f490dcc845e9f0643539d8649c
mejrakucevic <mejrakucevic420@gmail.com>
2024-07-19 16:42:34 +0200

    .

--- GAMELAUNCHER/main.py
+++ GAMELAUNCHER/main.py
@@ -1,45 +1,96 @@
-import tkinter as tk
+# import tkinter as tk
 
 from tkinter import *
+from PIL import Image, ImageTk
 
-root = tk.Tk()
+window = Tk()
 
-root.geometry("800x500")
-root.title("GameLauncher")
+window.geometry("540x540")
+window.title("LaunchX")
 
-label = tk.Label(root, text="Game Launcher", font=("Verdana", 18))
-label.pack(padx=20, pady=40)
+icon = PhotoImage(file="icon.png")
+window.iconphoto(True, icon)
 
-# textbox = tk.Text(root, height=3, font=("Arial", 15))
-# textbox.pack()
+window.config(background="#252626")
 
-# button = tk.Button(root, padx=30, pady=5, text="Start")
-# button.pack()
+photo = Image.open("photo.png")
+photoR = photo.resize((125, 125))
+photo = ImageTk.PhotoImage(photoR)
 
-btnFrame = tk.Frame(root)
-btnFrame.columnconfigure(0, weight=1)
-btnFrame.columnconfigure(1, weight=1)
-btnFrame.columnconfigure(2, weight=1)
+label = Label(window,
+              text="Mejra's Games",
+              font=("Roboto", 20),
+              fg="whitesmoke",
+              bg="#252626",
+              pady=5,
+              padx=20,
+              relief=RAISED,
+              bd=5,
+              )
+photoo = Label(window, image=photo, bg="#252626")
+label.pack()
+photoo.pack()
 
-btn1 = tk.Button(btnFrame,padx=30, pady=5, text="Start")
-btn1.grid(row=0, column=0,  padx=15, pady=15, sticky=tk.W+tk.E)
+def click():
+    print("Added!")
 
-btn2 = tk.Button(btnFrame,padx=30, pady=5, text="Start")
-btn2.grid(row=0, column=1, padx=15, pady=15, sticky=tk.W+tk.E)
+uploadImg = Image.open("upload.png")
+uploadR = uploadImg.resize((40,40))
+uploadImg = ImageTk.PhotoImage(uploadR)
 
-btn3 = tk.Button(btnFrame,padx=30, pady=5, text="Start")
-btn3.grid(row=0, column=2,  padx=15, pady=15, sticky=tk.W+tk.E)
+button = Button(window,
+                text="Add",
+                command=click,
+                font=("Roboto"),
+                bg="#ebebeb",
+                padx=25, pady=10,
+                relief=GROOVE,
+                activebackground="#6ccbeb",
+                image=uploadImg,
+                compound="right")
+button.place(x=200, y=250)
 
-btn1 = tk.Button(btnFrame,padx=30, pady=5, text="Start")
-btn1.grid(row=1, column=0,  padx=15, pady=15, sticky=tk.W+tk.E)
-
-btn2 = tk.Button(btnFrame,padx=30, pady=5, text="Start")
-btn2.grid(row=1, column=1, padx=15, pady=15, sticky=tk.W+tk.E)
-
-btn3 = tk.Button(btnFrame,padx=30, pady=5, text="Start")
-btn3.grid(row=1, column=2,  padx=15, pady=15, sticky=tk.W+tk.E)
-
-btnFrame.pack(fill='x')
+window.mainloop()
 
 
-root.mainloop()
+# root = tk.Tk()
+
+# root.geometry("800x500")
+# root.title("GameLauncher")
+
+# label = tk.Label(root, text="Game Launcher", font=("Verdana", 18))
+# label.pack(padx=20, pady=40)
+
+# # textbox = tk.Text(root, height=3, font=("Arial", 15))
+# # textbox.pack()
+
+# # button = tk.Button(root, padx=30, pady=5, text="Start")
+# # button.pack()
+
+# btnFrame = tk.Frame(root)
+# btnFrame.columnconfigure(0, weight=1)
+# btnFrame.columnconfigure(1, weight=1)
+# btnFrame.columnconfigure(2, weight=1)
+
+# btn1 = tk.Button(btnFrame,padx=30, pady=5, text="Start")
+# btn1.grid(row=0, column=0,  padx=15, pady=15, sticky=tk.W+tk.E)
+
+# btn2 = tk.Button(btnFrame,padx=30, pady=5, text="Start")
+# btn2.grid(row=0, column=1, padx=15, pady=15, sticky=tk.W+tk.E)
+
+# btn3 = tk.Button(btnFrame,padx=30, pady=5, text="Start")
+# btn3.grid(row=0, column=2,  padx=15, pady=15, sticky=tk.W+tk.E)
+
+# btn1 = tk.Button(btnFrame,padx=30, pady=5, text="Start")
+# btn1.grid(row=1, column=0,  padx=15, pady=15, sticky=tk.W+tk.E)
+
+# btn2 = tk.Button(btnFrame,padx=30, pady=5, text="Start")
+# btn2.grid(row=1, column=1, padx=15, pady=15, sticky=tk.W+tk.E)
+
+# btn3 = tk.Button(btnFrame,padx=30, pady=5, text="Start")
+# btn3.grid(row=1, column=2,  padx=15, pady=15, sticky=tk.W+tk.E)
+
+# btnFrame.pack(fill='x')
+
+
+# root.mainloop()
